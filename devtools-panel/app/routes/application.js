db8fc1635aa046463176ba92e7ca0d1ac4f20c81
Alexey Volkov <wsbaser@gmail.com>
2016-05-20 21:36:16 +0300

    VSClient implementation

--- devtools-panel/app/routes/application.js
+++ devtools-panel/app/routes/application.js
@@ -3,7 +3,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	vsclient: Ember.inject.service('vsclient'),
 	beforeModel(){
-		debugger;
 		var vsclient = this.get('vsclient');
 		vsclient.connect();
 
--- devtools-panel/app/services/vsclient.js
+++ devtools-panel/app/services/vsclient.js
@@ -1,19 +1,52 @@
 import Ember from 'ember';
 
+let SIMessageTypes = {
+	SessionWebData: "SessionWebData",
+	SessionWebRequest: "SessionWebRequest"
+};
+
+let SYNCHRONIZEIT_WS_URL = 'ws://localhost:18488/synchronize';
+
 export default Ember.Service.extend({
+	store: Ember.inject.service(),
 	connect(){
-		let webSocket = new WebSocket('ws://localhost:18488/synchronize');
-		webSocket.onopen = this.onopen;
-		webSocket.onmessage = this.onmessage;
+		let webSocket = new WebSocket(SYNCHRONIZEIT_WS_URL);
+		webSocket.onopen = this.onopen.bind(this);
+		webSocket.onmessage = this.onmessage.bind(this);
 		this.set('webSocket', webSocket);
 	},
-	onopen(event){
+	onopen(){
 		console.log('Socket is opened!');
+		this.sendSessionWebRequest();
 	},
 	onmessage(event){
-		console.log('Message received!', event);
+		console.log('Message received!');
+		var message = this.parseMessage(event.data);
+		if(message.Type===SIMessageTypes.SessionWebData){
+			this.updateStore(message.Data);
+		}else{
+			console.error('Received message with invalid message type:',message);
+		}
 	},
-	send(data){
-		this.get('webSocket').send(JSON.stringify(data));
+	sendSessionWebRequest(){
+		this.send(this.createMessage(SIMessageTypes.SessionWebRequest));
+	},
+	sendSessionWeb(sessionWeb){
+		this.send(this.createMessage(SIMessageTypes.SessionWebData, sessionWeb));
+	},
+	send(message){
+		this.get('webSocket').send(JSON.stringify(message));
+	},
+	parseMessage(data){
+		return JSON.parse(data);
+	},
+	createMessage(type, data){
+		return {
+			Type: type,
+			Data: data?JSON.stringify(data):''
+		};
+	},
+	updateStore(data){
+		this.get('store').pushPayload(data);
 	}
 });
