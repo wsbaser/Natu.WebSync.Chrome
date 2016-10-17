d7fe8fd4eb026fd4d02e1d0eda62ca898ab2488b
Alexey Volkov <wsbaser@gmail.com>
2016-10-17 19:53:52 +0300

    v 0.0.4

--- dev/js/background/index.js
+++ dev/js/background/index.js
@@ -1,19 +1,19 @@
-console.log('Background script executed');
-// Background page -- background.js
-chrome.runtime.onConnect.addListener(function(devToolsConnection) {
-    console.log('DevTools page connected');
-    // assign the listener function to a variable so we can remove it later
-    var devToolsListener = function(message, sender, sendResponse) {
-        // Inject a content script into the identified tab
-        console.log("Received message from DevTools page.");
-        console.log(message);
-        chrome.tabs.executeScript(message.tabId,
-            { file: message.scriptToInject });
-    }
-    // add the listener
-    devToolsConnection.onMessage.addListener(devToolsListener);
+// console.log('Background script executed');
+// // Background page -- background.js
+// chrome.runtime.onConnect.addListener(function(devToolsConnection) {
+//     console.log('DevTools page connected');
+//     // assign the listener function to a variable so we can remove it later
+//     var devToolsListener = function(message, sender, sendResponse) {
+//         // Inject a content script into the identified tab
+//         console.log("Received message from DevTools page.");
+//         console.log(message);
+//         chrome.tabs.executeScript(message.tabId,
+//             { file: message.scriptToInject });
+//     }
+//     // add the listener
+//     devToolsConnection.onMessage.addListener(devToolsListener);
 
-    devToolsConnection.onDisconnect.addListener(function() {
-         devToolsConnection.onMessage.removeListener(devToolsListener);
-    });
-});
+//     devToolsConnection.onDisconnect.addListener(function() {
+//          devToolsConnection.onMessage.removeListener(devToolsListener);
+//     });
+// });
