a74e1be57733df280f8587fdbc1d6090f5f45d63
Alexey Volkov <wsbaser@gmail.com>
2017-02-25 20:32:11 +0300

    Added link to base type in component-type model

--- devtools-panel/app/models/component-type.js
+++ devtools-panel/app/models/component-type.js
@@ -2,5 +2,11 @@ import DS from 'ember-data';
 import { hasMany } from 'ember-data/relationships';
 
 export default DS.Model.extend({
-	components: hasMany('component', { inverse: null })
+	baseComponentType: belongsTo('component-type', { inverse: null }),
+	components: hasMany('component', { inverse: null }),
+	allComponents: Ember.computed('components.[]', 'baseComponentType.components.[]', function(){
+	  	var components = this.get('components');
+	  	var baseComponents = this.get('baseComponentType.components') || [];
+	  	return components.concat(baseComponents);
+	})
 });
