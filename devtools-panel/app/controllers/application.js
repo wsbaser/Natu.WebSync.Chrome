5cce639b1dce6c8eedc704403f0c76ae27092ff8
Alexey Volkov <wsbaser@gmail.com>
2017-03-13 19:21:51 +0300

    Use observer for inputValue instead of key-up action

--- devtools-panel/app/controllers/application.js
+++ devtools-panel/app/controllers/application.js
@@ -9,24 +9,23 @@ export default Ember.Controller.extend({
 		var vsclient = this.get('vsclient');
 		vsclient.on("ConvertedSelector", this.onTargetSelectorReceived.bind(this));
 	},
+	onSourceSelectorChanged: Ember.observer('inputValue', function(){
+		var selector = this.get('inputValue');
+		var vsclient = this.get('vsclient');
+		console.log('KeyUp');
+		if(vsclient.get("isConnected")){
+			vsclient.convertSelector(selector);
+		}
+		else{
+			console.log('Convertion service not available. Using selector without conversion.');
+			this.set('targetCss', selector);
+			this.set('targetXPath', selector);
+		}		
+	}),
 	onTargetSelectorReceived(json){
 		var data = JSON.parse(json);
 		this.set('targetScss', data.Scss);
 		this.set('targetCss', data.Css);
 		this.set('targetXPath', data.XPath);
-	},
-	actions:{
-		onSourceSelectorChanged: function(selector) {
-			var vsclient = this.get('vsclient');
-			console.log('KeyUp');
-			if(vsclient.get("isConnected")){
-				vsclient.convertSelector(selector);
-			}
-			else{
-				console.log('Convertion service not available. Using selector without conversion.');
-				this.set('targetCss', selector);
-				this.set('targetXPath', selector);
-			}
-		}
 	}
 });
